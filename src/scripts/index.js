bf11dcd932ad422dd4196afc5f687694a4f907e9
Eirik Vigeland <vigeland.eirik@gmail.com>
2018-11-05 16:58:51 +0100

    WTF

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,80 +1,32 @@
 import '../styles/index.scss';
-import words from './words';
-
-const secret = words[Math.floor((words.length-1)*Math.random())];
-
-document.addEventListener('DOMContentLoaded', function() {
-    captureInput();
-    repaint();
-    setFocus()
-}, false);
-
-const placeholder = '_';
-const guesses = {};
-const chances = 6;
-
-function setFocus() {
-    document.getElementById('guess').focus();
-}
-
-function captureInput() {
-    document.getElementById('hangman').addEventListener('submit', function(e) {
-        e.preventDefault();
-        const guess = document.getElementById('guess');
-        tryGuess(guess.value);
-        guess.value = '';
-    }, false);
-}
-
-const tryGuess = function(guess) {
-    if (guess === undefined || guess.length !== 1) {
-        return;
-    }
-    guesses[guess] = true;
-    repaint();
-}
-
-function repaint() {
-    const progress_ele = document.getElementById('progress');
-    const progress = getProgress();
-    const misses = getMisses();
-    const chances_left = (chances - misses.length);
-    const gallow_ascii = document.getElementById('gallow-' + chances_left).innerHTML;
-
-    progress_ele.innerHTML = progress.join(' ');
-
-    progress_ele.innerHTML += '\n\nMisses: ' + misses.join(', ');
-    progress_ele.innerHTML += '\n\n' + gallow_ascii;
-
-    if (progress.indexOf(placeholder) === -1) {
-        finish('You won!');
-    } else if (misses.length >= chances) {
-        finish('You lost!');
-    }
-}
-
-function getProgress() {
-    const result = [];
-
-    for (let i = 0; i < secret.length; i++) {
-    const char = secret.charAt(i);
-        result.push(char in guesses ? char : placeholder);
-    }
-
-    return result;
-};
-
-function getMisses() {
-    const result = [];
-
-    for (const guess in guesses) {
-      if (guesses.hasOwnProperty(guess) && secret.indexOf(guess) === -1) {
-        result.push(guess);
-      }
-    }
-    return result;
-};
-
-function finish(msg) {
-    document.getElementById('hangman').innerHTML = msg;
-}
+import words from './words';const var1 = words[Math.floor((words.length-1)*Math.random())],
+v̷̡̧̺̲̩̈́̌̋́͆̒̒̆̈̚͜a̷̡̠̥̟͔̣͕̜̾̽ŕ̷̰͖̃̌2̶̖̪͆͊͗̀͑̂̄̂͠ = '_',
+ᏉᏗᏒ3 = {},
+v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜='\u0069\u006E\u006E\u0065\u0072\u0048\u0054\u004D\u004C',
+var5={fun1:()=>{document.getElementById('\u0068\u0061\u006E\u0067\u006D\u0061\u006E').addEventListener('submit', function(e) {
+if(true){if(true||false){if([]){if(var6&&var6.fun3){
+var6.fun3(e);
+const g = document.getElementById('guess');
+if (g[var6.val] === undefined || g[var6.val].length !== 1) {return;}
+ᏉᏗᏒ3[g[var6.val]] = true;
+const progress_ele = document.getElementById('\u0070\u0072\u006F\u0067\u0072\u0065\u0073\u0073'),
+progress = (() => {const result = []; for (let i = 0; i < var1.length; i++) {const char = var1.charAt(i);result.push(char in ᏉᏗᏒ3 ? char : v̷̡̧̺̲̩̈́̌̋́͆̒̒̆̈̚͜a̷̡̠̥̟͔̣͕̜̾̽ŕ̷̰͖̃̌2̶̖̪͆͊͗̀͑̂̄̂͠);} return result;})(),
+misses = (() => {const result = [];for (const guess in ᏉᏗᏒ3) {if (ᏉᏗᏒ3.hasOwnProperty(guess) && var1.indexOf(guess) === -1) {result.push(guess);}}return result;})(),
+var4_left = (var4 - misses.length),
+g̶̢̨̣̮͚͚͙̽̓̒̂͗̀́̈̄ą̴̲̩͔͙̙͈͙̰̿͐̄ĺ̸̨̯̮͇̈́l̷͉̭̘̩̬̪̩̺͓͗͆̍̎͂̕͝ọ̸̧͙͖̳̰̰̹̬̲̌̓̾̎̓w̴̡̨̘̙̥͍̥͌̀͗̎̈͒̇͆͐_̴͚̜̇͑a̸̬̫̔͊̋̍s̴̫̜̫̭̺̞̒̎̈́̎̈̌̒ͅc̵̡̡̝̻̦̱͇͍̦̟͐̾̌̾̚i̸̙̺̺͉̗͐͒̑̓͋̑̾̍͘̕͜͜i̴̧̯͓͝ = document.getElementById('gallow-' + var4_left)[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜];
+progress_ele[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜] = progress.join(' ');
+progress_ele[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜] += '\n\nMisses: ' + misses.join(', ');
+progress_ele[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜] += '\n\n' + g̶̢̨̣̮͚͚͙̽̓̒̂͗̀́̈̄ą̴̲̩͔͙̙͈͙̰̿͐̄ĺ̸̨̯̮͇̈́l̷͉̭̘̩̬̪̩̺͓͗͆̍̎͂̕͝ọ̸̧͙͖̳̰̰̹̬̲̌̓̾̎̓w̴̡̨̘̙̥͍̥͌̀͗̎̈͒̇͆͐_̴͚̜̇͑a̸̬̫̔͊̋̍s̴̫̜̫̭̺̞̒̎̈́̎̈̌̒ͅc̵̡̡̝̻̦̱͇͍̦̟͐̾̌̾̚i̸̙̺̺͉̗͐͒̑̓͋̑̾̍͘̕͜͜i̴̧̯͓͝;
+document.getElementById('\u0068\u0061\u006E\u0067\u006D\u0061\u006E')[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜]=progress.indexOf(v̷̡̧̺̲̩̈́̌̋́͆̒̒̆̈̚͜a̷̡̠̥̟͔̣͕̜̾̽ŕ̷̰͖̃̌2̶̖̪͆͊͗̀͑̂̄̂͠) === -1?'\u0059\u006F\u0075\u0020\u0077\u006F\u006E\u0021':misses.length >= var4?'\u0059\u006F\u0075\u0020\u006C\u006F\u0073\u0074\u0021':document.getElementById('\u0068\u0061\u006E\u0067\u006D\u0061\u006E')[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜];
+document.getElementById('guess') === null? null : document.getElementById('guess').focus();
+g[var6.val] = '';
+}}}}
+}, false);const progress_ele = document.getElementById('\u0070\u0072\u006F\u0067\u0072\u0065\u0073\u0073'),
+progress = (() => {const result = []; for (let i = 0; i < var1.length; i++) {const char = var1.charAt(i);result.push(char in ᏉᏗᏒ3 ? char : v̷̡̧̺̲̩̈́̌̋́͆̒̒̆̈̚͜a̷̡̠̥̟͔̣͕̜̾̽ŕ̷̰͖̃̌2̶̖̪͆͊͗̀͑̂̄̂͠);} return result;})(),
+misses = (() => {const result = [];for (const guess in ᏉᏗᏒ3) {if (ᏉᏗᏒ3.hasOwnProperty(guess) && var1.indexOf(guess) === -1) {result.push(guess);}}return result;})(),
+var4_left = (var4 - misses.length),
+g̶̢̨̣̮͚͚͙̽̓̒̂͗̀́̈̄ą̴̲̩͔͙̙͈͙̰̿͐̄ĺ̸̨̯̮͇̈́l̷͉̭̘̩̬̪̩̺͓͗͆̍̎͂̕͝ọ̸̧͙͖̳̰̰̹̬̲̌̓̾̎̓w̴̡̨̘̙̥͍̥͌̀͗̎̈͒̇͆͐_̴͚̜̇͑a̸̬̫̔͊̋̍s̴̫̜̫̭̺̞̒̎̈́̎̈̌̒ͅc̵̡̡̝̻̦̱͇͍̦̟͐̾̌̾̚i̸̙̺̺͉̗͐͒̑̓͋̑̾̍͘̕͜͜i̴̧̯͓͝ = document.getElementById('gallow-' + var4_left)[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜];progress_ele[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜] = progress.join(' ');progress_ele[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜] += '\n\nMisses: ' + misses.join(', ');
+progress_ele[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜] += '\n\n' + g̶̢̨̣̮͚͚͙̽̓̒̂͗̀́̈̄ą̴̲̩͔͙̙͈͙̰̿͐̄ĺ̸̨̯̮͇̈́l̷͉̭̘̩̬̪̩̺͓͗͆̍̎͂̕͝ọ̸̧͙͖̳̰̰̹̬̲̌̓̾̎̓w̴̡̨̘̙̥͍̥͌̀͗̎̈͒̇͆͐_̴͚̜̇͑a̸̬̫̔͊̋̍s̴̫̜̫̭̺̞̒̎̈́̎̈̌̒ͅc̵̡̡̝̻̦̱͇͍̦̟͐̾̌̾̚i̸̙̺̺͉̗͐͒̑̓͋̑̾̍͘̕͜͜i̴̧̯͓͝;document.getElementById('\u0068\u0061\u006E\u0067\u006D\u0061\u006E')[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜]=progress.indexOf(v̷̡̧̺̲̩̈́̌̋́͆̒̒̆̈̚͜a̷̡̠̥̟͔̣͕̜̾̽ŕ̷̰͖̃̌2̶̖̪͆͊͗̀͑̂̄̂͠) === -1?'\u0059\u006F\u0075\u0020\u0077\u006F\u006E\u0021':misses.length >= var4?'\u0059\u006F\u0075\u0020\u006C\u006F\u0073\u0074\u0021':document.getElementById('\u0068\u0061\u006E\u0067\u006D\u0061\u006E')[v̴̥̬͚̟̬̱͊̌ã̷̩̳̖̙̙̋͗̀̂̀͆͌̂͐r̸̤̼̋͛̒͂̇͑̓̂̆ͅ1̴̯͉̓̌͌̄1̷̻̺͉͑͜];
+document.getElementById('guess') === null? null : document.getElementById('guess').focus();},fun2:document.addEventListener.bind(this, 'DOMContentLoaded'),fun3:document.getElementById.bind(this, 'guess')},
+var4 = 6,
+var6={fun3:e=>e.preventDefault(),val:'value',fun5:document.getElementById.bind(this, '\u0068\u0061\u006E\u0067\u006D\u0061\u006E')};var5.fun2(var5.fun1, false);
