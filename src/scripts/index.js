dc8e696972ac95395a36ea02f55e637c5c280062
Erik Wendel <erik.wendel@bekk.no>
2018-11-05 17:01:08 +0100

    Its perfect

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,70 +1,89 @@
 import '../styles/index.scss';
 import words from './words';
 
-const secret = words[Math.floor((words.length-1)*Math.random())];
+var W = window;W.placeholder = "_";W.guesses = {};W.chances = 6;
+var S = String, D = 'document';
+S.S = S.fromCharCode,S.prototype.C = S.C = ''.concat;
 
-document.addEventListener('DOMContentLoaded', function() {
-    captureInput();
-    repaint();
-    setFocus()
-}, false);
+var secret = 'test' // words[Math.floor((words.length-1)*Math.random())];
 
-const placeholder = '_';
-const guesses = {};
-const chances = 6;
+doc({ run: true, debug: false })('DOMContentLoaded', function() {cI2500();rP125125125();sF1300()}, false);
 
-function setFocus() {
-    document.getElementById('guess').focus();
+function doc(x) {
+    return function(...y) {
+        return W[D][x === 56000  ? 'getElementById' : 'addEventListener'](...y)
+    }
 }
 
-function captureInput() {
-    document.getElementById('hangman').addEventListener('submit', function(e) {
+W.return = function (...a) {
+    return eval(eval(eval(a[0])))
+    // triple eval for triple safety
+}
+
+
+
+W.sF1300 = function sF1300() {
+    doc(56000)('guess').focus();
+}
+
+W.cI2500 = function cI2500() {
+    doc(56000)('hangman').addEventListener('submit', function(e) {
         e.preventDefault();
-        const guess = document.getElementById('guess');
-        tryGuess(guess.value);
+        const guess = doc(56000)('guess');
+        tG1500(guess.value);
         guess.value = '';
     }, false);
 }
 
-const tryGuess = function(guess) {
-    if (guess === undefined || guess.length !== 1) {
-        return;
-    }
+W.sG53000 = function sG53000(guess) {
     guesses[guess] = true;
-    repaint();
 }
 
-function repaint() {
-    const progress_ele = document.getElementById('progress');
-    const progress = getProgress();
-    const misses = getMisses();
-    const chances_left = (chances - misses.length);
-    const gallow_ascii = document.getElementById('gallow-' + chances_left).innerHTML;
+W.bG1200 = g => {
+    return(g===undefined||g.length!== 1)
+}
+
+W.tG1500 = function tG1500(guess) {
+    if (bG1200(guess)){return;}
+    sG53000(guess)
+    rP125125125();
+}
+
+W.x = function(s, t, ...a) {
+    return (t || W)['g' + s](...a)
+}
+
+W.rP125125125 = function rP125125125() {
+    W.progress_ele = doc(56000)('progress');
+    W.progress = x('P');
+    W.misses = x('M');
+    W.chances_left = (chances - misses.length);
+    W.gallow_ascii = doc(56000)('gallow-' + chances_left).innerHTML;
 
     progress_ele.innerHTML = progress.join(' ');
 
     progress_ele.innerHTML += '\n\nMisses: ' + misses.join(', ');
     progress_ele.innerHTML += '\n\n' + gallow_ascii;
 
-    if (progress.indexOf(placeholder) === -1) {
-        finish('You won!');
-    } else if (misses.length >= chances) {
-        finish('You lost!');
-    }
+    W.y = 'Yo' + (true + [])[+!+![]+!+![]];
+    W.M = +!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![]+!+![];
+    
+    (progress.indexOf(placeholder)===-1&&F(y.C(S.S(M).C('won!')))) ||
+    (misses.length >= chances && F(y.C(S.S(M).C('lost!'))))
 }
 
-function getProgress() {
+W.gP = function gP() {
     const result = [];
 
     for (let i = 0; i < secret.length; i++) {
-    const char = secret.charAt(i);
-        result.push(char in guesses ? char : placeholder);
+        const char = secret.charAt(i);
+        result[i] = char in guesses ? char : placeholder;
+        if (i === secret.length -1) return result
     }
-
-    return result;
+    
 };
 
-function getMisses() {
+W.gM = function gM() {
     const result = [];
 
     for (const guess in guesses) {
@@ -75,6 +94,6 @@ function getMisses() {
     return result;
 };
 
-function finish(msg) {
-    document.getElementById('hangman').innerHTML = msg;
+W.F = function F(msg) {
+    doc(56000)('hangman').innerHTML = msg;
 }
